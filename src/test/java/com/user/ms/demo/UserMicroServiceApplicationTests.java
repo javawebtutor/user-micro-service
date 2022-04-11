d5ecde736938344cb5b07245c45e707d458e3994
mrajnitro <mrajnitro@gmail.com>
2022-04-12 01:44:41 +0530

    Initial commit

---
+++ src/test/java/com/user/ms/demo/UserMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.ms.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
